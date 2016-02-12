aae62fde23c75ff63594cb732efe828fc12dfa6e
gérald m <geraldm@imac-de-gerald.local>
2016-02-12 13:55:15 +0100

    Initial Commit

---
+++ Core Data Project/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Core Data Project
+//
+//  Created by gérald m on 12/02/2016.
+//  Copyright © 2016 gérald m. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Core Data Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core Data Project
+//
+//  Created by gérald m on 12/02/2016.
+//  Copyright © 2016 gérald m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
